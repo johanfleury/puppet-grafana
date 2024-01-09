dd34e1fdcd9c14f0969359f494e9bbe33ec1a920
jbond <github@johnbond.org>
2024-01-09 17:13:54 +0100

    settings: Add additional parameters to log and server settings
    Add support for log.filters and server.serve_from_sub_path

--- manifests/settings/server.pp
+++ manifests/settings/server.pp
@@ -11,19 +11,21 @@ class grafana::settings::server (
   Optional[Boolean] $enable_gzip = undef,
   Optional[String] $cert_file = undef,
   Optional[String] $cert_key = undef,
+  Optional[Boolean] $serve_from_sub_path = undef,
 ) {
   $settings = {
-    'protocol'         => $protocol,
-    'http_addr'        => $http_addr,
-    'http_port'        => $http_port,
-    'domain'           => $domain,
-    'enforce_domain'   => $enforce_domain,
-    'root_url'         => $root_url,
-    'router_logging'   => $router_logging,
-    'static_root_path' => $static_root_path,
-    'enable_gzip'      => $enable_gzip,
-    'cert_file'        => $cert_file,
-    'cert_key'         => $cert_key,
+    'protocol'            => $protocol,
+    'http_addr'           => $http_addr,
+    'http_port'           => $http_port,
+    'domain'              => $domain,
+    'enforce_domain'      => $enforce_domain,
+    'root_url'            => $root_url,
+    'router_logging'      => $router_logging,
+    'static_root_path'    => $static_root_path,
+    'enable_gzip'         => $enable_gzip,
+    'cert_file'           => $cert_file,
+    'cert_key'            => $cert_key,
+    'serve_from_sub_path' => $serve_from_sub_path,
   }
 
   ::grafana::settings { 'server': settings => $settings }
